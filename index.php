7508a89c34f0553026ddf2767e8e1c4603abcb07
Luciano Rodrigo Toniani <44039148+Ghostdovahkin@users.noreply.github.com>
2019-07-14 19:02:23 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/'>
-    
-    <title>Document</title>
-
-</head>
-<body>
-    
-</body>
+<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel='stylesheet prefetch' href=''>
+<link href="css/style.css" rel="stylesheet" id="bootstrap-css">
 <script src="js/chat.js"></script>
-</html>
--- login.php
+++ login.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
         <div class="row">
         <div class="col-sm-4">
         <h4>Chat Login:</h4>
         <form method="POST">
         <div class="form-group">
-        <?php if($loginError){ ?>
-        <div class="alert alert-warning"><?php echo $loginError; ?></div>
+        <?php if($loginError) { ?>
+        <div class="alert alert-warning">
+        <?php echo $loginError; ?>
+        </div>
         <?php } ?>
         </div>
         <div class="form-group">
@@ -28,5 +22,3 @@
         </form>
         </div>
         </div>
-</body>
-</html>
